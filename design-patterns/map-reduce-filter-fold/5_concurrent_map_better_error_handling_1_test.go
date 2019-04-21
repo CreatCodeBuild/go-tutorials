227f9328ec70e70430480a0605f2af4accb7719e
CreatCodeBuild <CreatCodeBuild@github.com>
2019-04-21 11:53:47 -0700

    more examples

--- design-patterns/map-reduce-filter-fold/5_concurrent_map_better_error_handling_1_test.go
+++ design-patterns/map-reduce-filter-fold/5_concurrent_map_better_error_handling_1_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"testing"
@@ -17,19 +18,36 @@ func (e *ErrorChannel) Error() string {
 	return fmt.Sprintf("There are %d errors in this channel.", e.Count)
 }
 
+func NewErrorChannel(errs chan error, count int) error {
+	errCh := &ErrorChannel{
+		Errs: make(chan error, count),
+	}
+	for i := 0; i < count; i++ {
+		e := <-errs
+		if e != nil {
+			errCh.Errs <- e
+			errCh.Count++
+		}
+	}
+	if errCh.Count == 0 {
+		return nil
+	}
+	return errCh
+}
+
 func ConcurrentMapBetterErrorHandling(p genericProducer, c genericConsumer, mapper genericMapper) error {
 	count := 0
-	errs := make(chan error)
+	errs := make(chan error, 1)
 	for {
-		count++
 		next, err := p.Next()
 		if err != nil {
 			if err == io.EOF {
 				break // There is no more elements in the producer.
 			}
 			errs <- err
-			return &ErrorChannel{Errs: retErrs, Count: count}// There is an error in the producer. Shut down the mapping.
+			return NewErrorChannel(errs, count+1) // There is an error in the producer. Shut down the mapping.
 		}
+		count++
 		go func(next interface{}) {
 			ele, err := mapper(next)
 			if err != nil {
@@ -44,17 +62,48 @@ func ConcurrentMapBetterErrorHandling(p genericProducer, c genericConsumer, mapp
 			errs <- nil
 		}(next)
 	}
-	return &ErrorChannel{Errs: errs, Count: count}
+	return NewErrorChannel(errs, count)
+}
+
+func TestConcurrentMapBetterErrorHandling(t *testing.T) {
+	t.Run("all correct", func(t2 *testing.T) {
+		results2 := outputConsumer2{}
+		err := ConcurrentMapBetterErrorHandling(NewIntProducer(1, 2, 3, 4, 5), &results2, func(x interface{}) (interface{}, error) {
+			return x, nil
+		})
+		require.NoError(t, err)
+	})
+
+	t.Run("all producer errors", func(t2 *testing.T) {
+		results2 := outputConsumer2{}
+		err := ConcurrentMapBetterErrorHandling(errorProducer(5), &results2, func(x interface{}) (interface{}, error) {
+			return x, nil
+		})
+		require.NoError(t, err)
+	})
 }
 
 // type genericProducer interface {
 // 	Next() (interface{}, error)
 // }
 
+// NextFunc is a function type which implements a one method interface that has the same signature.
 type NextFunc func() (interface{}, error)
 
+// Next calls the function itself.
 func (f NextFunc) Next() (interface{}, error) {
 	return f()
+}
+
+func errorProducer(n int) NextFunc {
+	i := 0
+	return func() (interface{}, error) {
+		if i < n {
+			defer func() { i++ }()
+			return nil, errors.New("some random error")
+		}
+		return nil, io.EOF
+	}
 }
 
 func NewIntProducer(slice ...int) NextFunc {
@@ -76,6 +125,7 @@ func NewIntProducer(slice ...int) NextFunc {
 	}
 }
 
+// MultiProducer merges multiple producers into one. You can find the same design in io.MultiReader and io.MultiWriter, though their implementations are different.
 func MultiProducer(ps ...producer) NextFunc {
 	type result struct {
 		value interface{}
@@ -98,12 +148,4 @@ func MultiProducer(ps ...producer) NextFunc {
 			return 0, io.EOF
 		}
 	}
-}
-
-func TestConcurrentMapBetterErrorHandling(t *testing.T) {
-	results2 := outputConsumer2{}
-	err := ConcurrentMapBetterErrorHandling(NewIntProducer(1, 2, 3), &results2, func(x interface{}) (interface{}, error) {
-		return x, nil
-	})
-	require.NoError(t, err)
 }
